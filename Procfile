65c4dd811a39447315364b979daaa885ab8bc795
Adi <adrian.rus@live.com>
2016-06-07 12:05:07 +0100

    Initial deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn stream2-project:app
---
+++ stream2-project.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask import render_template
+from pymongo import MongoClient
+import json
+
+
+app = Flask(__name__)
+
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+DBS_NAME = 'donorsUSA'
+COLLECTION_NAME = 'projects'
+FIELDS = {'funding_status': True, 'school_state': True, 'resource_type': True,
+          'poverty_level': True, 'date_posted': True, 'total_donations': True,
+          'primary_focus_area':True, '_id': False}
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/home.html')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/donorsUS/projects')
+def donor_projects():
+    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+    collection = connection[DBS_NAME][COLLECTION_NAME]
+    projects = collection.find(projection=FIELDS, limit=80000)
+    json_projects = []
+    for project in projects:
+        json_projects.append(project)
+    json_projects = json.dumps(json_projects)
+    connection.close()
+    return json_projects
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
